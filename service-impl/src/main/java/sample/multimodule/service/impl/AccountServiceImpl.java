582952e547c2f7f339c46575bdd56209640b4c45
Harish Kadamudi <harishyouk@gmail.com>
2016-05-30 12:12:25 +0530

    calling JMS in service

--- service-impl/src/main/java/sample/multimodule/service/impl/AccountServiceImpl.java
+++ service-impl/src/main/java/sample/multimodule/service/impl/AccountServiceImpl.java
@@ -5,58 +5,66 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import sample.multimodule.domain.entity.Account;
+import sample.multimodule.domain.xml.AccountXML;
 import sample.multimodule.repository.AccountRepository;
 import sample.multimodule.service.api.AccountNotFoundException;
 import sample.multimodule.service.api.AccountService;
+import sample.multimodule.xml.api.XmlService;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    @Value("${dummy.type}")
-    private String dummyType;
+	@Value("${dummy.type}")
+	private String dummyType;
 
-    @Autowired
-    private AccountRepository accountRepository;
+	@Autowired
+	private AccountRepository accountRepository;
 
-    /**
-     * {@inheritDoc}
-     * <p/>
-     * Dummy method for testing purposes.
-     * 
-     * @param number The account number. Set 0000 to get an {@link AccountNotFoundException} 
-     */
-    @Override
-    public Account findOne(String number) throws AccountNotFoundException {
-        if(number.equals("0000")) {
-            throw new AccountNotFoundException("0000");
-        }
-        
-        Account account = accountRepository.findByNumber(number);
-        if(account == null){
-          account = createAccountByNumber(number);
-        }
+	@Autowired
+	private XmlService xmlService;
 
-        return account;
-    }
+	/**
+	 * {@inheritDoc}
+	 * <p/>
+	 * Dummy method for testing purposes.
+	 * 
+	 * @param number
+	 *            The account number. Set 0000 to get an
+	 *            {@link AccountNotFoundException}
+	 */
+	@Override
+	public Account findOne(String number) throws AccountNotFoundException {
+		if (number.equals("0000")) {
+			throw new AccountNotFoundException("0000");
+		}
 
-    @Override
-    public Account createAccountByNumber(String number) {
-        Account account = new Account();
-        account.setNumber(number);
-        return accountRepository.save(account);
-    }
+		Account account = accountRepository.findByNumber(number);
+		if (account == null) {
+			account = createAccountByNumber(number);
+		}
 
-    public String getDummyType() {
-        return dummyType;
-    }
+		return account;
+	}
 
-    public void setDummyType(String dummyType) {
-        this.dummyType = dummyType;
-    }
+	@Override
+	public Account createAccountByNumber(String number) {
+		Account account = new Account();
+		account.setNumber(number);
+		return accountRepository.save(account);
+	}
+
+	public String getDummyType() {
+		return dummyType;
+	}
+
+	public void setDummyType(String dummyType) {
+		this.dummyType = dummyType;
+	}
 
 	@Override
 	public Account save(Account account) {
-//		return null;
+		// return null;
+		AccountXML xml = xmlService.getXML(account, new AccountXML());
 		return accountRepository.save(account);
 	}
 
